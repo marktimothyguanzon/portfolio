21b6a130505c9dcf1a4e689e4ffbdf0cb8dd8d12
Mark Timothy T. Guanzon <marktimothy19861986@gmail.com>
2025-11-13 13:12:11 +0800

    updated website

--- kapengpinoy_backup/kapengpinoy/script.js
+++
@@ -1,9 +0,0 @@
-// Example: Add smooth scroll for internal links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e){
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
